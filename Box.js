3515fccb93e788b4d3703ede958d07f97b07bc78
Athrvapatil-123 <67160004+Athrvapatil-123@users.noreply.github.com>
2020-10-03 12:28:04 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+class Box{
+    constructor (x,y,width,height){
+        var options={
+            isSatic:true
+        }
+        this.body = Matter.Bodies.rectangle(x,y,width,height,options)            
+        Matter.World.add(world,this.body)
+        this.width=width;
+        this.height=height
+    }
+    display(){
+        fill("red")
+        rectMode(CENTER)
+        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+    }
+}
